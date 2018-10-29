f42c83238840b39d3c8d1baf5640f7387fa4b127
hoang-nguyen-vn <hoangnhonline@gmail.com>
2018-10-29 17:08:00 +0700

    first

---
+++ app/Http/Routes/backend.php
@@ -0,0 +1,89 @@
+<?php
+// Authentication routes...
+Route::get('backend/login', ['as' => 'backend.login-form', 'uses' => 'Backend\UserController@loginForm']);
+Route::post('backend/login', ['as' => 'backend.check-login', 'uses' => 'Backend\UserController@checkLogin']);
+Route::get('backend/logout', ['as' => 'backend.logout', 'uses' => 'Backend\UserController@logout']);
+Route::group(['namespace' => 'Backend', 'prefix' => 'backend', 'middleware' => 'isAdmin'], function()
+{    
+    Route::group(['prefix' => 'compare'], function () {
+        Route::get('/', ['as' => 'compare.index', 'uses' => 'CompareController@index']);
+    });
+    Route::group(['prefix' => 'settings'], function () {
+        Route::get('/', ['as' => 'settings.index', 'uses' => 'SettingsController@index']);
+        Route::post('/update', ['as' => 'settings.update', 'uses' => 'SettingsController@update']);
+    });
+    Route::group(['prefix' => 'report'], function () {
+        Route::get('/', ['as' => 'report.index', 'uses' => 'ReportController@index']);     
+        Route::post('/search-price-other-site', ['as' => 'crawler.search-price-other-site', 'uses' => 'CompareController@search']);
+    });
+    
+    Route::group(['prefix' => 'pages'], function () {
+        Route::get('/', ['as' => 'pages.index', 'uses' => 'PagesController@index']);
+        Route::get('/create', ['as' => 'pages.create', 'uses' => 'PagesController@create']);
+        Route::post('/store', ['as' => 'pages.store', 'uses' => 'PagesController@store']);
+        Route::get('{id}/edit',   ['as' => 'pages.edit', 'uses' => 'PagesController@edit']);
+        Route::post('/update', ['as' => 'pages.update', 'uses' => 'PagesController@update']);
+        Route::get('{id}/destroy', ['as' => 'pages.destroy', 'uses' => 'PagesController@destroy']);
+    });
+    
+    Route::group(['prefix' => 'gift'], function () {
+        Route::get('/', ['as' => 'gift.index', 'uses' => 'GiftController@index']);
+        Route::get('/create', ['as' => 'gift.create', 'uses' => 'GiftController@create']);
+        Route::post('/store', ['as' => 'gift.store', 'uses' => 'GiftController@store']);
+        Route::get('{id}/edit',   ['as' => 'gift.edit', 'uses' => 'GiftController@edit']);
+        Route::post('/update', ['as' => 'gift.update', 'uses' => 'GiftController@update']);
+        Route::get('{id}/destroy', ['as' => 'gift.destroy', 'uses' => 'GiftController@destroy']);
+    });
+    Route::group(['prefix' => 'newsletter'], function () {
+        Route::get('/', ['as' => 'newsletter.index', 'uses' => 'NewsletterController@index']);
+        Route::post('/store', ['as' => 'newsletter.store', 'uses' => 'NewsletterController@store']);
+        Route::get('{id}/edit',   ['as' => 'newsletter.edit', 'uses' => 'NewsletterController@edit']);
+        Route::get('/export',   ['as' => 'newsletter.export', 'uses' => 'NewsletterController@download']);
+        Route::post('/update', ['as' => 'newsletter.update', 'uses' => 'NewsletterController@update']);
+        Route::get('{id}/destroy', ['as' => 'newsletter.destroy', 'uses' => 'NewsletterController@destroy']);
+    });
+    Route::group(['prefix' => 'contact'], function () {
+        Route::get('/', ['as' => 'contact.index', 'uses' => 'ContactController@index']);
+        Route::post('/store', ['as' => 'contact.store', 'uses' => 'ContactController@store']);
+        Route::get('{id}/edit',   ['as' => 'contact.edit', 'uses' => 'ContactController@edit']);
+        Route::get('/export',   ['as' => 'contact.export', 'uses' => 'ContactController@download']);
+        Route::post('/update', ['as' => 'contact.update', 'uses' => 'ContactController@update']);
+        Route::get('{id}/destroy', ['as' => 'contact.destroy', 'uses' => 'ContactController@destroy']);
+    });
+    Route::post('/tmp-upload', ['as' => 'image.tmp-upload', 'uses' => 'UploadController@tmpUpload']);
+    Route::post('/tmp-upload-multiple', ['as' => 'image.tmp-upload-multiple', 'uses' => 'UploadController@tmpUploadMultiple']);
+    Route::post('/update-order', ['as' => 'update-order', 'uses' => 'GeneralController@updateOrder']);
+    Route::post('/ck-upload', ['as' => 'ck-upload', 'uses' => 'UploadController@ckUpload']);
+    Route::post('/get-slug', ['as' => 'get-slug', 'uses' => 'GeneralController@getSlug']);
+    Route::post('/cap-nhat-thu-tu', ['as' => 'cap-nhat-thu-tu', 'uses' => 'GeneralController@updateOrderList']);
+
+     Route::group(['prefix' => 'articles-cate'], function () {
+        Route::get('/', ['as' => 'articles-cate.index', 'uses' => 'ArticlesCateController@index']);
+        Route::get('/create', ['as' => 'articles-cate.create', 'uses' => 'ArticlesCateController@create']);
+        Route::post('/store', ['as' => 'articles-cate.store', 'uses' => 'ArticlesCateController@store']);
+        Route::get('{id}/edit',   ['as' => 'articles-cate.edit', 'uses' => 'ArticlesCateController@edit']);
+        Route::post('/update', ['as' => 'articles-cate.update', 'uses' => 'ArticlesCateController@update']);
+        Route::get('{id}/destroy', ['as' => 'articles-cate.destroy', 'uses' => 'ArticlesCateController@destroy']);
+    });
+    
+    Route::group(['prefix' => 'account'], function () {
+        Route::get('/', ['as' => 'account.index', 'uses' => 'AccountController@index']);
+        Route::get('/change-password', ['as' => 'account.change-pass', 'uses' => 'AccountController@changePass']);
+        Route::post('/store-password', ['as' => 'account.store-pass', 'uses' => 'AccountController@storeNewPass']);
+        Route::get('/update-status/{status}/{id}', ['as' => 'account.update-status', 'uses' => 'AccountController@updateStatus']);
+        Route::get('/create', ['as' => 'account.create', 'uses' => 'AccountController@create']);
+        Route::post('/store', ['as' => 'account.store', 'uses' => 'AccountController@store']);
+        Route::get('{id}/edit',   ['as' => 'account.edit', 'uses' => 'AccountController@edit']);
+        Route::post('/update', ['as' => 'account.update', 'uses' => 'AccountController@update']);
+        Route::get('{id}/destroy', ['as' => 'account.destroy', 'uses' => 'AccountController@destroy']);
+    });
+    Route::group(['prefix' => 'articles'], function () {
+        Route::get('/', ['as' => 'articles.index', 'uses' => 'ArticlesController@index']);
+        Route::get('/create', ['as' => 'articles.create', 'uses' => 'ArticlesController@create']);
+        Route::post('/store', ['as' => 'articles.store', 'uses' => 'ArticlesController@store']);
+        Route::get('{id}/edit',   ['as' => 'articles.edit', 'uses' => 'ArticlesController@edit']);
+        Route::post('/update', ['as' => 'articles.update', 'uses' => 'ArticlesController@update']);
+        Route::get('{id}/destroy', ['as' => 'articles.destroy', 'uses' => 'ArticlesController@destroy']);
+    });
+
+});
---
+++ resources/views/backend/partials/sidebar.blade.php
@@ -0,0 +1,85 @@
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="{{ URL::asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>{{ Auth::user()->full_name }}</p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
+    <!-- /.search form -->
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu">
+      <li class="header">MAIN NAVIGATION</li>      
+      <li class="treeview {{ in_array(\Request::route()->getName(), ['gift.index', 'gift.create', 'gift.edit']) ? 'active' : '' }}">
+        <a href="#">
+          <i class="fa fa-pencil-square-o"></i> 
+          <span>Giải thưởng</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li {{ in_array(\Request::route()->getName(), ['gift.edit', 'gift.index']) ? "class=active" : "" }}><a href="{{ route('gift.index') }}"><i class="fa fa-circle-o"></i> Danh sách quà</a></li>
+          <li {{ in_array(\Request::route()->getName(), ['gift.create']) ? "class=active" : "" }} ><a href="{{ route('gift.create') }}"><i class="fa fa-circle-o"></i> Thêm quà</a></li>        
+        </ul>
+       
+      </li>
+      <li class="treeview {{ in_array(\Request::route()->getName(), ['articles.index', 'articles.create', 'articles.edit','articles-cate.create', 'articles-cate.index', 'articles-cate.edit']) ? 'active' : '' }}">
+        <a href="#">
+          <i class="fa fa-pencil-square-o"></i> 
+          <span>Nội dung</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li {{ in_array(\Request::route()->getName(), ['articles.edit', 'articles.index']) ? "class=active" : "" }}><a href="{{ route('articles.index') }}"><i class="fa fa-circle-o"></i> Nội dung</a></li>
+          <li {{ in_array(\Request::route()->getName(), ['articles.create']) ? "class=active" : "" }} ><a href="{{ route('articles.create', ['cate_id' => 1]) }}"><i class="fa fa-circle-o"></i> Thêm nội dung</a></li>
+          <li {{ in_array(\Request::route()->getName(), ['articles-cate.create', 'articles-cate.index', 'articles-cate.edit']) ? "class=active" : "" }} ><a href="{{ route('articles-cate.index') }}"><i class="fa fa-circle-o"></i> Danh mục</a></li>          
+        </ul>
+       
+      </li>
+      <li {{ in_array(\Request::route()->getName(), ['newsletter.edit', 'newsletter.index']) ? "class=active" : "" }}>
+        <a href="{{ route('newsletter.index') }}">
+          <i class="fa fa-pencil-square-o"></i> 
+          <span>Newsletter</span>         
+        </a>       
+      </li>
+      <li {{ in_array(\Request::route()->getName(), ['contact.edit', 'contact.index']) ? "class=active" : "" }}>
+        <a href="{{ route('contact.index') }}">
+          <i class="fa fa-pencil-square-o"></i> 
+          <span>Liên hệ</span>          
+        </a>       
+      </li>
+      
+      <li class="treeview {{ in_array(\Request::route()->getName(), ['loai-thuoc-tinh.index', 'thuoc-tinh.index', 'color.index']) ? 'active' : '' }}">
+        <a href="#">
+          <i class="fa  fa-gears"></i>
+          <span>Cài đặt</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li {{ \Request::route()->getName() == "settings.index" ? "class=active" : "" }}><a href="{{ route('settings.index') }}"><i class="fa fa-circle-o"></i> Thông tin tiecngon.vn</a></li>        
+          <li {{ \Request::route()->getName() == "account.index" ? "class=active" : "" }}><a href="{{ route('account.index') }}"><i class="fa fa-circle-o"></i> Users</a></li>          
+        </ul>
+      </li>
+      <!--<li class="header">LABELS</li>
+      <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+      <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+      <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>-->
+    </ul>
+  </section>
+  <!-- /.sidebar -->
+</aside>
+<style type="text/css">
+  .skin-blue .sidebar-menu>li>.treeview-menu{
+    padding-left: 15px !important;
+  }
+</style>
