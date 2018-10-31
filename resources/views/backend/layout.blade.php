ae5e7b77f6d20f7e16e92c7cc25f02df32b394c1
hoang-nguyen-vn <hoangnhonline@gmail.com>
2018-10-31 10:37:26 +0700

    fix

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -45,7 +45,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 4.0.0
     </div>
-    <strong>Copyright &copy; 2018 <a href="mailto:hoangnhonline@gmail.com">hoangnhonline@gmail.com</a>.</strong> All rights
+    <strong>Copyright &copy; 2018.</strong> All rights
     reserved.
   </footer>
 
