2f09be39464267616169ed630d280402f8ba2672
hoang-nguyen-vn <hoangnhonline@gmail.com>
2018-10-31 08:57:23 +0700

    fix

--- resources/views/backend/contact/index.blade.php
+++ resources/views/backend/contact/index.blade.php
@@ -54,7 +54,7 @@
         
         <!-- /.box-header -->
         <div class="box-body">
-        <a href="{{ route('contact.export') }}" class="btn btn-info btn-sm" style="margin-bottom:5px;float:right" target="_blank">Export</a>
+        <!-- <a href="{{ route('contact.export') }}" class="btn btn-info btn-sm" style="margin-bottom:5px;float:right" target="_blank">Export</a> -->
           <div style="text-align:center">
             {{ $items->appends( ['status' => $status, 'email' => $email, 'phone' => $phone] )->links() }}
           </div>  
